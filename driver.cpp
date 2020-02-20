e8534334f7a23b2dfa55d399fb56562a867b0bfd
Alex Soldin <alex.soldin@gmail.com>
2020-02-20 17:59:07 +0200

    Created driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ios>
+
+#include "VolImage.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+
+    return 0;
+}
