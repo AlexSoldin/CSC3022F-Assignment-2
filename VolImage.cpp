30a92f8d4620a2efd7de83cd53684fa35bd90951
Alex Soldin <alex.soldin@gmail.com>
2020-02-20 17:37:57 +0200

    Initial commit. Cpp file and header file have been created

---
+++ VolImage.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+// #include <cstdlib>
+// #include <ios>
+
+#include "VolImage.h"
+
+using namespace std;
+
+SLDALE003::VolImage::VolImage(){
+    width=0;
+    height=0;
+}
