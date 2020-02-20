6ba319b95a21a37bb97d69bfaa2a5fe5c3a84f04
Alex Soldin <alex.soldin@gmail.com>
2020-02-20 17:42:11 +0200

    Setup header file and completed both contstructor and destructor

--- VolImage.cpp
+++ VolImage.cpp
@@ -3,8 +3,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
-// #include <cstdlib>
-// #include <ios>
+#include <cstdlib>
+#include <ios>
 
 #include "VolImage.h"
 
@@ -13,4 +13,13 @@ using namespace std;
 SLDALE003::VolImage::VolImage(){
     width=0;
     height=0;
+}
+
+SLDALE003::VolImage::~VolImage(){
+    for(int slice = 0; slice < slices.size(); slice++){
+        for(int i = 0; i < height; i ++){
+            delete [] slices[slice][i];
+        }
+        delete [] slices[slice];
+    }
 }
---
+++ VolImage.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ios>
+
+#ifndef _VOLIMAGE_CLASS_H
+#define _VOLIMAGE_CLASS_H
+
+
+#endif
